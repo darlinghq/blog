a9070f8b77cc22024aa4e80ed83df88117331bde
Ariel Abreu <facekapow@outlook.com>
2023-05-26 23:45:40 -0400

    Add remark-github plugin
    This allows us to use `@` user references and `#` issue and PR references like we would in normal GitHub Markdown.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,90 +3,103 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-	title: 'Darling Development Blog',
-	tagline: 'macOS translation layer for Linux',
-	favicon: 'img/favicon.ico',
+/** @returns {Promise<import('@docusaurus/types').Config>} */
+async function createConfig() {
+	const { default: remarkGithub } = await import('remark-github');
 
-	url: 'https://blog.darlinghq.com',
-	baseUrl: '/',
+	return {
+		title: 'Darling Development Blog',
+		tagline: 'macOS translation layer for Linux',
+		favicon: 'img/favicon.ico',
 
-	// GitHub pages deployment config.
-	organizationName: 'darlinghq',
-	projectName: 'blog',
+		url: 'https://blog.darlinghq.com',
+		baseUrl: '/',
 
-	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
+		// GitHub pages deployment config.
+		organizationName: 'darlinghq',
+		projectName: 'blog',
 
-	i18n: {
-		defaultLocale: 'en',
-		locales: ['en'],
-	},
+		onBrokenLinks: 'throw',
+		onBrokenMarkdownLinks: 'warn',
 
-	presets: [
-		[
-			'classic',
-			/** @type {import('@docusaurus/preset-classic').Options} */
+		i18n: {
+			defaultLocale: 'en',
+			locales: ['en'],
+		},
+
+		presets: [
+			[
+				'classic',
+				/** @type {import('@docusaurus/preset-classic').Options} */
+				({
+					docs: false,
+					blog: {
+						blogTitle: 'Darling Development Blog',
+						blogDescription: 'macOS translation layer for Linux',
+						routeBasePath: '/',
+						showReadingTime: true,
+						editUrl: 'https://github.com/darlinghq/blog/tree/main/',
+						postsPerPage: 'ALL',
+						blogSidebarTitle: 'All posts',
+						blogSidebarCount: 'ALL',
+						remarkPlugins: [
+							[remarkGithub, {
+								repository: 'darlinghq/darling',
+							}],
+						],
+						feedOptions: {
+							type: 'all',
+							copyright: `Copyright © ${new Date().getFullYear()} Darling developers`,
+						},
+					},
+					theme: {
+						customCss: require.resolve('./src/css/custom.css'),
+					},
+				}),
+			],
+		],
+
+		themeConfig:
+			/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 			({
-				docs: false,
-				blog: {
-					blogTitle: 'Darling Development Blog',
-					blogDescription: 'macOS translation layer for Linux',
-					routeBasePath: '/',
-					showReadingTime: true,
-					editUrl: 'https://github.com/darlinghq/blog/tree/main/',
-					postsPerPage: 'ALL',
-					blogSidebarTitle: 'All posts',
-					blogSidebarCount: 'ALL',
+				// Replace with your project's social card
+				//image: 'img/darling-social-card.jpg',
+				navbar: {
+					title: 'Darling Development Blog',
+					logo: {
+						alt: 'Darling',
+						src: 'img/logo.svg',
+					},
+					items: [
+						{
+							href: 'https://github.com/darlinghq/darling',
+							label: 'GitHub',
+							position: 'right',
+						},
+					],
 				},
-				theme: {
-					customCss: require.resolve('./src/css/custom.css'),
+				footer: {
+					style: 'dark',
+					links: [
+						{
+							title: 'More',
+							items: [
+								{
+									label: 'GitHub',
+									href: 'https://github.com/darlinghq/darling',
+								},
+							],
+						},
+					],
+					copyright: `Copyright © ${new Date().getFullYear()} Darling developers. Built with Docusaurus.`,
+				},
+				prism: {
+					theme: lightCodeTheme,
+					darkTheme: darkCodeTheme,
+					additionalLanguages: ['cmake', 'nasm'],
 				},
 			}),
-		],
-	],
-
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			// Replace with your project's social card
-			//image: 'img/darling-social-card.jpg',
-			navbar: {
-				title: 'Darling Development Blog',
-				logo: {
-					alt: 'Darling',
-					src: 'img/logo.svg',
-				},
-				items: [
-					{
-						href: 'https://github.com/darlinghq/darling',
-						label: 'GitHub',
-						position: 'right',
-					},
-				],
-			},
-			footer: {
-				style: 'dark',
-				links: [
-					{
-						title: 'More',
-						items: [
-							{
-								label: 'GitHub',
-								href: 'https://github.com/darlinghq/darling',
-							},
-						],
-					},
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} Darling developers. Built with Docusaurus.`,
-			},
-			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
-				additionalLanguages: ['cmake', 'nasm'],
-			},
-		}),
+	};
 };
 
-module.exports = config;
+module.exports = createConfig;
